a69be4e3c090450b848c20f50ecc5c1accdde3c3
Michael May <maym86@gmail.com>
2018-12-17 00:06:31 -0800

    Cleanera way for reading the bag data

--- src/lidar_camera/src/lidar_camera_node.cpp
+++ src/lidar_camera/src/lidar_camera_node.cpp
@@ -1,10 +1,17 @@
 #include "lidar_camera_node.h"
 
 #include <ros/ros.h>
+
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
+#include <message_filters/subscriber.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/sync_policies/approximate_time.h>
+
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
 
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_types.h>
@@ -12,27 +19,29 @@
 
 pcl::visualization::CloudViewer viewer ("Simple Cloud Viewer");
 
-void lidarCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
-{
-   //https://answers.ros.org/question/136916/conversion-from-sensor_msgspointcloud2-to-pclpointcloudt/
+using namespace sensor_msgs;
+using namespace message_filters;
 
-    pcl::PCLPointCloud2 pcl_pc2;
-    pcl_conversions::toPCL(*msg, pcl_pc2);
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
 
-    pcl::fromPCLPointCloud2(pcl_pc2, *cloud);
-    viewer.showCloud(cloud);
-}
+void callback(const ImageConstPtr& image_msg, const CameraInfoConstPtr& cam_info_msg, const PointCloud2ConstPtr& lidar_msg) {
 
-void imageCallback(const sensor_msgs::ImageConstPtr& msg) {
     try {
-        cv::Mat image = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8)->image;
+        cv::Mat image = cv_bridge::toCvShare(image_msg, image_encodings::BGR8)->image;
         cv::imshow("view", image);
         cv::waitKey(1);
     }
     catch (cv_bridge::Exception& e) {
         ROS_ERROR("Could not convert to image!");
     }
+
+    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+    pcl::fromROSMsg(*lidar_msg, *cloud);
+    viewer.showCloud(cloud);
+
+
+
+
+
 }
 
 
@@ -44,11 +53,14 @@ int main(int argc, char **argv) {
     cv::namedWindow("view");
     cv::startWindowThread();
 
-    ros::Subscriber image_sub = node_handle.subscribe("/sensors/camera/image_rect", 2, imageCallback);
-    ros::Subscriber lidar_sub = node_handle.subscribe("/sensors/velodyne_points", 2, lidarCallback);
+    Subscriber<Image> image_sub(node_handle, "/sensors/camera/image_rect", 2);
+    Subscriber<CameraInfo> info_sub(node_handle, "/sensors/camera/camera_info", 2);
+    Subscriber<PointCloud2> lidar_sub(node_handle, "/sensors/velodyne_points", 2);
+
+    typedef sync_policies::ApproximateTime<Image, CameraInfo, PointCloud2> SyncPolicy;
+    Synchronizer<SyncPolicy> sync(SyncPolicy(10), image_sub, info_sub, lidar_sub);
+    sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
     ros::spin();
     cv::destroyWindow("view");
-
-
 }
