2ba40138512ef5fc31ba992edfb5ab1a1a583066
Michael May <maym86@gmail.com>
2018-12-16 16:55:07 -0800

    Set up lidar camera calibration module as package

---
+++ src/lidar_camera/include/lidar_camera_node.h
@@ -0,0 +1,7 @@
+#ifndef LIDAR_CAMERA_NODE_H
+#define LIDAR_CAMERA_NODE_H
+
+#include "ros/ros.h"
+
+
+#endif
---
+++ src/lidar_camera/test/test_lidar_camera.cpp
@@ -0,0 +1,8 @@
+#include "lidar_camera_node.h"
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "lidar_camera");
+
+
+}
